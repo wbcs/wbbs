ef1aa6019cdf8f574166ba6a0bf65fb59cfb3ccb
王冰 <wangbing.cs@bytedance.com>
2020-02-25 15:14:04 +0800

    feat: first commit

---
+++ wbbs/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wbbs project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wbbs.settings')
+
+application = get_wsgi_application()
